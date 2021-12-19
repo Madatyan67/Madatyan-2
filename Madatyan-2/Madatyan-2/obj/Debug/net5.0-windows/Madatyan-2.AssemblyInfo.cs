979b026cd66d33b2b86ffca0f29646b892f204dc
Madatyan67 <96314593+Madatyan67@users.noreply.github.com>
2021-12-19 17:23:08 +0300

    Update Madatyan-2.AssemblyInfo.cs

--- Madatyan-2/Madatyan-2/obj/Debug/net5.0-windows/Madatyan-2.AssemblyInfo.cs
+++ Madatyan-2/Madatyan-2/obj/Debug/net5.0-windows/Madatyan-2.AssemblyInfo.cs
@@ -11,12 +11,12 @@
 using System;
 using System.Reflection;
 
-[assembly: System.Reflection.AssemblyCompanyAttribute("Artak02")]
+[assembly: System.Reflection.AssemblyCompanyAttribute("Madatyan02")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
-[assembly: System.Reflection.AssemblyProductAttribute("Artak02")]
-[assembly: System.Reflection.AssemblyTitleAttribute("Artak02")]
+[assembly: System.Reflection.AssemblyProductAttribute("Madatyan02")]
+[assembly: System.Reflection.AssemblyTitleAttribute("Madatyan02")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
 [assembly: System.Runtime.Versioning.TargetPlatformAttribute("Windows7.0")]
 [assembly: System.Runtime.Versioning.SupportedOSPlatformAttribute("Windows7.0")]
